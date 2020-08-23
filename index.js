e8a1544fce45fd1a63f4e26ebf4850850ea65441
Zeeshan Ahmad <zeeshan.official@gmail.com>
2020-08-24 01:47:02 +0530

    Database and schema setup with routes defined

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
 
-app.get('/', (req, res) => 
-res.send(`Node and express server running on port ${PORT}`)
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.get('/', (request, response) => 
+response.send(`Node and express server running on port ${PORT}`)
 );
 
 app.listen(PORT, () => 
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,25 @@
+import { addNewContact } from '../controllers/crmController';
+
+const routes = (app) => {
+    app.route('/contact')
+        .get((request, response, next) => {
+            //middleware
+            console.log(`Request from: ${request.originalUrl}`)
+            console.log(`Rquest type: ${request.method}`)
+            next();
+        }, (request, response, next) => {
+            response.send('GET request successful!')
+        })
+
+        .post((request, response) =>
+        response.send('POST request successful!'));
+
+    app.route('/contact/:contactID')
+        .put((request, response) => 
+        response.send('PUT request successful!'))
+
+        .delete((request, response) => 
+        response.send('DELETE request successful!'));
+}
+
+export default routes;
